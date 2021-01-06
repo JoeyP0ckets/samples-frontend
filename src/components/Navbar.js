52980d63d8e2a48fbaa4480a0d7ce41a526f79a4
github username <github email address>
2021-01-06 15:45:35 -0600

    changed some css styling on homepage

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ const Navbar = () => {
   return (
     <div>
       <Router>
-          <div className="navbar-row">
+          <div className="navbar-container">
           <h1 className="navbar-logo">First Dose</h1>
           <Link to="/">Home</Link>&nbsp;&nbsp;&nbsp;
           <Link to="/Samples">Samples List</Link>&nbsp;&nbsp;&nbsp;
