3bfcb97270ea69de7c17455d3b82b7905826d1ba
github username <github email address>
2021-01-07 17:18:30 -0600

    imported bootstrap and made basic sample cards

--- src/containers/SampleCard.js
+++ src/containers/SampleCard.js
@@ -1,12 +1,25 @@
 import React from 'react'
+import {Card, CardImg, Button} from 'react-bootstrap'
 
 const SampleCard = (props) => {
   return (
-    <div>
-        <img src={props.sample.image_url} alt={props.sample.sample_name}/>
-        {props.sample.sample_name}
-    </div>
+    <Card style={{ width: '18rem' }}>
+    <Card.Img variant="top" src={props.sample.image_url} alt={props.sample.sample_name} />
+    <Card.Body>
+      <Card.Title>{props.sample.sample_name}</Card.Title>
+      <Card.Text>
+        Some quick example text to build on the card title and make up the bulk of
+        the card's content.
+      </Card.Text>
+      <Button variant="primary">Place Order</Button>
+    </Card.Body>
+  </Card>
   )
 }
 
-export default SampleCard
+export default SampleCard 
+
+    // <div>
+    //     <img src={props.sample.image_url} alt={props.sample.sample_name}/>
+    //     {props.sample.sample_name}
+    // </div>
