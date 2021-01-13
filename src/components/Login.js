d8975aee5b6e55157ac74fa2743078e6bafdb6af
github username <github email address>
2021-01-13 11:20:04 -0600

    created login component - not plugged in

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,27 @@
 import React from "react"
+import { Form, Button } from "react-bootstrap"
 
 const Login = () => {
-  return(
-    <div>
-      I'm the Login/Signup Page
-    </div>
+
+  const handleLoginSubmit = e => {
+    e.preventDefault()
+    const user = {
+      user: {
+        username: e.target.username.value,
+        password: e.target.password.value
+      }
+    }
+  }
+  
+    return(
+    <Form onSubmit={e => handleLoginSubmit(e)}>
+    <Form.Group>
+     <Form.Control type="text" placeholder="Username" name="username"/>  
+     <Form.Control type="password" placeholder="Password" name="password"/>   
+     <br></br>
+     <Button type="submit">Login</Button>
+    </Form.Group>
+ </Form>
   )
 }
 
