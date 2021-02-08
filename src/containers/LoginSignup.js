9b9ffb566aa4e71a04a371b8ae099c5c12ada582
github username <github email address>
2021-02-08 17:09:28 -0600

    row and columns in LoginSignup.js

--- src/containers/LoginSignup.js
+++ src/containers/LoginSignup.js
@@ -1,14 +1,22 @@
 import React from "react"
 import Login from '../components/Login'
 import Signup from '../components/Signup'
+import { Row, Col } from 'react-bootstrap'
 
 const LoginSignup = () => {
   return (
-    <div>
-      Already Created an Account?
-      <Login/>
-      Signup For An Account
-      <Signup/>
+    <div className="login-container">
+      <Row>
+        <Col xs={6} md={6}>
+        <h3 className="review-text">Login</h3>
+        <Login/>
+        </Col>
+        <Col xs={6} md={6}>
+        <h3 className="review-text" style={{ color: "black"}}>Signup</h3>
+        <Signup/>
+        </Col>
+      </Row>
+
     </div>
   )
 }
