dfc6f35daf6f0ba9cb0fb3c98999ebb2226c4b7a
github username <github email address>
2021-02-08 16:49:42 -0600

    changed component tree in navbar display

---
+++ src/containers/LoginSignup.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Login from '../components/Login'
+import Signup from '../components/Signup'
+
+const LoginSignup = () => {
+  return (
+    <div>
+      <Login/>
+      <Signup/>
+    </div>
+  )
+}
+
+export default LoginSignup
