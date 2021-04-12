e47fa00222505c0d09e8f9879eca0793e08821e0
github username <github email address>
2021-04-11 23:48:04 -0500

    added to welcome text

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,7 +3,9 @@ import React from "react"
 const Welcome = () => {
   return (
     <div className="welcome-page">
-      <h1>Welcome to First Dose.  Your first choice in sample ordering</h1>
+      <h1>Welcome to First Dose.  Your first choice in sample ordering. ADS is committed to providing its clients with the 
+          highest quality in bronchial asthmatic medication.  Create and account to order free samples of our latest offerings
+      </h1>
       <button>Enter Site</button>
     </div>
   )
