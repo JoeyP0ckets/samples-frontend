cd26a30c940f50c34062a5fa9ba52b664677e86b
github username <github email address>
2021-04-15 15:10:35 -0500

    changed redirect from auth token

--- src/components/RetrieveAuthToken.js
+++ src/components/RetrieveAuthToken.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import $ from 'jquery'
+import { useHistory } from 'react-router-dom'
 
 const RetrieveAuthToken = () => {
+
+  const history = useHistory();
   const auth_code = localStorage.getItem('docusign_auth_code')
   
   $.ajax({
     url: "http://localhost:3000/api/v1/docusign/retrieve_docusign_token",
     type: "post",
-    data: { auth_code: JSON.stringify(auth_code)}
+    data: { auth_code: auth_code }
   })
+      history.push("/Samples");
       return null;
     }
 
