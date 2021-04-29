a1d50d5919396cfeeec15f26ee4bb73f10945c6b
github username <github email address>
2021-04-29 16:39:07 -0500

    changed Authcode for Implicit Grant fetch

--- src/components/RetrieveAuthCode.js
+++ src/components/RetrieveAuthCode.js
@@ -3,11 +3,12 @@ import queryString from "query-string"
 
 const RetrieveAuthCode = () => {
   const history = useHistory();
-  const { search } = useLocation();
-  const { code } = queryString.parse(search);
+  const { hash } = useLocation();
+  const { access_token } = queryString.parse(hash);
+  console.log(hash)
 
-  localStorage.setItem('docusign_auth_code', code);
-  history.push('/Get-Token');
+  localStorage.setItem('docusign_auth_code', access_token);
+  history.push('/');
   return null;
 };
 
