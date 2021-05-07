b11d5457f3222bf34825498f9cb4d02384b67969
github username <github email address>
2021-05-07 15:06:38 -0500

    added fetch to contain session token to backend. refactored ajax to no longer be needed

--- src/components/RetrieveUserInfo.js
+++ src/components/RetrieveUserInfo.js
@@ -1,17 +1,35 @@
-import $ from 'jquery'
+import React from "react"
 import { useHistory } from 'react-router-dom'
 
 const RetrieveUserInfo = () => {
 
   const history = useHistory();
   const access_token = localStorage.getItem('docusign_access_token')
+
+  const fetchUser = React.useCallback(() => {
+    const token = localStorage.getItem('auth_token')
+
+    if(!token) {
+      return
+    }
+
+    const fetchObj = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Auth-Token': token
+      },
+      body: JSON.stringify({access_token: access_token })
+    }
+
+    fetch(`http://localhost:3000/api/v1/docusign/retrieve_user_info`, fetchObj)
+    .then(resp => resp.json())
+    .then(resp => console.log(resp))
+  }, [])
+
+   React.useEffect(fetchUser, [])
   
-  $.ajax({
-    url: "http://localhost:3000/api/v1/docusign/retrieve_user_info",
-    type: "post",
-    data: { access_token: access_token }
-  })
-      history.push("/create_envelope");
+      history.push("/");
       return null;
     }
 
