eced897c716a7dd53ba0e6800c0eefeabd24a6cd
github username <github email address>
2022-01-25 13:36:10 -0600

    docusign fetch

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -24,7 +24,7 @@ const Navbar = (props) => {
   let access_token = localStorage.getItem('docusign_access_token')
 
   if (props.user && !access_token) {
-    console.log("I don't have a docusign token")
+    window.location.href = 'https://account-d.docusign.com/oauth/auth?response_type=token&client_id=95849d5d-a7e9-4572-bb38-d1efdd1d1a38&redirect_uri=http://localhost:3001/retrieve_auth_code/&scope=signature'
   }
   return (
     <div>
