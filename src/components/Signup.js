4cfa5d81c71bdd8be598c24b7478455fcb73f116
github username <github email address>
2022-02-03 13:42:28 -0600

    changed to heroku docusign fetch

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,4 +1,4 @@
-import React, {useState, useDispatch, useContext } from "react"
+import React, {useState, useContext } from "react"
 import { Form, Button} from 'react-bootstrap'
 import { NavLink } from "react-router-dom"
 import { AuthContext }from '../context/AuthProvider'
