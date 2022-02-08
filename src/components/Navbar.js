72ab779b1e119d2f51c27df10c3105abc6d9c816
github username <github email address>
2022-02-08 10:37:06 -0600

    heroku docusign fetch

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -24,8 +24,8 @@ const Navbar = (props) => {
   let access_token = localStorage.getItem('docusign_access_token')
 
   if (props.user && !access_token) {
-    console.log("I don't have a docusign token")
-    // window.location.href = 'https://account-d.docusign.com/oauth/auth?response_type=token&client_id=95849d5d-a7e9-4572-bb38-d1efdd1d1a38&redirect_uri=https://first-dose-fill.herokuapp.com/retrieve_auth_code/&scope=signature'
+    // console.log("I don't have a docusign token")
+    window.location.href = 'https://account-d.docusign.com/oauth/auth?response_type=token&client_id=95849d5d-a7e9-4572-bb38-d1efdd1d1a38&redirect_uri=https://first-dose-fill.herokuapp.com/retrieve_auth_code/&scope=signature'
   }
   return (
     <div>
